38f469efb25c5bf1ef1b8c2037f2fbab0530ef49
Swapnil <84333762+Swapnil-programmer7@users.noreply.github.com>
2022-06-26 02:13:17 +0530

    HTML CSS added, body parser and express active. site runs on local host for now

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/', function(req, res) {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    console.log(firstName, lastName, email);
+});
 //Setting the port
 app.listen(3000, function(res,req){
   console.log('Server is running on port 3000');
